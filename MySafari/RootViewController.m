80aa5d7423b16bb14a877610a0a708058159a88e
EvanCV <evan.c.vandenberg@gmail.com>
2015-01-18 20:51:13 -0800

    updated search bar and UI

--- MySafari/RootViewController.m
+++ MySafari/RootViewController.m
@@ -8,40 +8,65 @@
 
 #import "RootViewController.h"
 
-@interface RootViewController () <UIWebViewDelegate, UITextFieldDelegate, UIAlertViewDelegate, UIScrollViewDelegate>
+@interface RootViewController () <UIWebViewDelegate, UITextFieldDelegate, UIAlertViewDelegate, UIScrollViewDelegate, UISearchBarDelegate>
+
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 @property (weak, nonatomic) IBOutlet UITextField *urlTextField;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 @property (weak, nonatomic) IBOutlet UIButton *backwardButton;
 @property (weak, nonatomic) IBOutlet UIButton *forwardButton;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UIScrollView *theScrollView;
+@property CGPoint originalCenter;
+
 
 @end
+
+//Make sure to note the urlTextField object is set to hidden, can be reactivated by clicking the hidden button in the attributes inspector
 
 @implementation RootViewController
 
 - (void)viewDidLoad {
+
     [super viewDidLoad];
-    [self loadNewWebPage:@"http://theageofmammals.com/burgers"];
+    [self loadNewWebPage:@"http://yahoo.com"];
+
+    //UI enhancements
     self.spinner.hidden = TRUE;
     self.backwardButton.enabled = FALSE;
     self.forwardButton.enabled = FALSE;
     self.webView.scrollView.delegate = self;
+    self.urlTextField.backgroundColor = [UIColor whiteColor];
+    self.urlTextField.hidden = TRUE;
 
-    self.urlTextField.backgroundColor = [UIColor clearColor];
+    //search bar changes
+    self.searchBar.backgroundColor = [UIColor clearColor];
+    self.searchBar.barTintColor = [UIColor orangeColor];
+    self.searchBar.tintColor = [UIColor blueColor];
+
+    //find the original center position of the search bar
+    self.searchBar.center = self.originalCenter;
 }
 
+
+//Essentially this method is manually turning a textfield into a search bar
 - (void)loadNewWebPage:(NSString *)string
 {
     NSString *addressString = string;
+
     if (![string hasPrefix:@"http://"])
     {
         addressString = [NSString stringWithFormat:@"http://%@", string];
     }
+
     NSURL *adddressURL = [NSURL URLWithString:addressString];
     NSURLRequest *addressRequest = [NSURLRequest requestWithURL:adddressURL];
+
     [self.webView loadRequest:addressRequest];
 }
 
+
+//Show spinning loading icon when webpages are loading
 - (void)webViewDidStartLoad:(UIWebView *)webView
 {
     [self.spinner startAnimating];
@@ -50,14 +75,19 @@
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView
 {
+    //Stopping the loading icon and hiding it when not loading
     self.spinner.hidden = TRUE;
     [self.spinner stopAnimating];
 
+    //setting the nav bar title to the webpage name/title
     NSString *theTitle=[webView stringByEvaluatingJavaScriptFromString:@"document.title"];
     self.navigationItem.title = theTitle;
-    
 
-    //Enabling/disabling backward button depending on whether the user can back to a previous page
+    //self.backwardButton.enabled = self.webView.canGoBack;
+
+
+
+    //Enabling/disabling backward button depending on whether the user can go back to a previous page
     if ([self.webView canGoBack])
     {
         self.backwardButton.enabled = TRUE;
@@ -85,12 +115,14 @@
     self.urlTextField.text = currentURL.absoluteString;
 }
 
+//Enabling the return button to trigger action
 -(BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     [self loadNewWebPage:textField.text];
     return TRUE;
 }
 
+//Setting all of the functionality of the bottom bar buttons
 - (IBAction)onReloadButtonPressed:(id)sender
 {
     [self.webView reload];
@@ -111,6 +143,7 @@
     [self.webView goForward];
 }
 
+//Arbitrary button made to inform users that a new feature is "coming soon"
 - (IBAction)onPlusButtonPressed:(UIButton *)sender
 {
     UIAlertView *alertView = [UIAlertView new];
@@ -131,13 +164,41 @@
     {
         // react to dragging down
         self.urlTextField.hidden = TRUE;
+        self.navigationController.navigationBar.alpha = 0.0;
+        //[self.navigationController.navigationBar setTranslucent:YES];
     }
     else
     {
         // react to dragging up
         self.urlTextField.hidden = FALSE;
+        self.navigationController.navigationBar.alpha = 1.0;
+        //[self.navigationController.navigationBar setTranslucent:YES];
     }
 }
+
+-(void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
+{
+    [self loadNewWebPage:searchText];
+}
+
+- (IBAction)handlePan:(UIPanGestureRecognizer *)gesture
+{
+    CGPoint touchPoint  = [gesture locationInView:self.view];
+    self.searchBar.center = touchPoint;
+}
+
+
+- (void)keyboardWasShown:(UIKeyboardAppearance *)notification
+{
+    if (notification)
+    {
+    [UIView animateWithDuration:.5 animations:^
+    {
+        self.searchBar.center = self.originalCenter;
+    }];
+    }
+}
+
 
 
 @end
